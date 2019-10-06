f2496d15f327aa76193711b71b98271475fd23c8
sushie <sascha_burku@yahoo.de>
2019-10-06 18:18:38 +0200

    Add types based on schema

---
+++ app/graphql/types/department_type.rb
@@ -0,0 +1,18 @@
+module Types
+  class DepartmentType < Types::BaseObject
+    field :id, ID, 'The unique id of the department', null: false
+    field :name, String, 'The name of the department',  null: true
+    field :location,
+          String,
+          'The name of the phyiscal location of this department',
+          null: true
+    field :employees,
+          [EmployeeType, null: true],
+          'A list of employees working in this department',
+          null: true
+
+    def employees
+      object.employees
+    end
+  end
+end
---
+++ app/graphql/types/employee_type.rb
@@ -0,0 +1,25 @@
+module Types
+  class EmployeeType < Types::BaseObject
+    field :id, ID, 'The unique id of the employee', null: false
+    field :name, String, 'The name of the employee',  null: true
+    field :age, Integer, 'The age of the employee', null: true
+    field :yearlySalary,
+          Float,
+          'The salary of a year the employee earns',
+          null: true,
+          method: :yearly_salary
+    field :workStart,
+          String,
+          'The first working day of the employee',
+          null: true,
+          method: :work_start
+    field :departments,
+          [DepartmentType, null: true],
+          'A list of departments this employee belongs to',
+          null: true
+
+    def departments
+      object.departments
+    end
+  end
+end
