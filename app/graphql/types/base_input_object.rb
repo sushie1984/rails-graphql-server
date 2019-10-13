e03a22ab2dd24f11245e4ed1a010ea6c35fa4aca
sushie <sascha_burku@yahoo.de>
2019-10-13 20:36:21 +0200

    Do not invoke base params class

--- app/graphql/types/base_input_object.rb
+++ app/graphql/types/base_input_object.rb
@@ -1,6 +1,5 @@
 module Types
   class BaseInputObject < GraphQL::Schema::InputObject
-    argument_class Types::BaseArgument
     argument :first, Integer, null: true, as: :limit
     argument :offset, Integer, null: true
   end
