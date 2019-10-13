f2b9901b8b9d93df3969a87d2144150955de8a26
sushie <sascha_burku@yahoo.de>
2019-10-13 20:28:54 +0200

    Move arguments to explicit class

--- app/graphql/types/base_argument.rb
+++ app/graphql/types/base_argument.rb
@@ -1,6 +1,4 @@
 module Types
   class BaseArgument < GraphQL::Schema::Argument
-    argument :first, Integer, null: true, as: :limit
-    argument :offset, Integer, null: true
   end
 end
--- app/graphql/types/base_input_object.rb
+++ app/graphql/types/base_input_object.rb
@@ -1,5 +1,7 @@
 module Types
   class BaseInputObject < GraphQL::Schema::InputObject
     argument_class Types::BaseArgument
+    argument :first, Integer, null: true, as: :limit
+    argument :offset, Integer, null: true
   end
 end
