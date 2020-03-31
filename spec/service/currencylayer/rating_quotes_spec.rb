2ba288e4e9b5d19a591dd3e2cba542345994387b
Sascha Burku <sascha_burku@yahoo.de>
2020-03-31 11:46:08 +0200

    Spec upsert target rates case

--- spec/service/currencylayer/rating_quotes_spec.rb
+++ spec/service/currencylayer/rating_quotes_spec.rb
@@ -29,6 +29,23 @@ RSpec.describe Currencylayer::RatingQuotes do
         expect { upsert }
           .not_to change { Currency.where(source: source).count }
       end
+
+      context 'with a newer rates' do
+        let(:fixed_update_rates) { [{ 'target' => 'USD', 'rate' => 1.1 }] }
+        let(:upsert_fixed_rates) do
+          described_class.new(source,
+                              fixed_update_rates,
+                              published_at,
+                              target_currencies).upsert
+        end
+
+        it 'updates target_rates of currency entry' do
+          expect { upsert_fixed_rates }
+            .to change { Currency.find_by(source: source).target_rates }
+            .from(update_rates)
+            .to(fixed_update_rates)
+        end
+      end
     end
   end
 end
