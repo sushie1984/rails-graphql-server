6edfc3e558da8781ec5be61b7b39938eb9b34dda
Sascha Burku <sascha_burku@yahoo.de>
2020-04-04 21:15:05 +0200

    Render exchange rate via partial xhr request

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -7,9 +7,42 @@ class CurrenciesController < ApplicationController
     @currency = currency
   end
 
+  def exchange
+    @foreign_rate =
+      { 'target' => foreign_currency, 'result' => foreign_rate * Float(value) }
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
   private
+
+  def foreign_rate
+    CurrencyExchanger.new(base_currency, foreign_rates)
+                     .foreign_rate_for(foreign_currency)
+  end
+
+  def base_currency
+    currency_params[:base]
+  end
+
+  def foreign_rates
+    currency.target_rates
+  end
+
+  def foreign_currency
+    currency_params[:foreign]
+  end
+
+  def value
+    currency_params[:value]
+  end
 
   def currency
     @currency ||= Currency.find(params[:id])
+  end
+
+  def currency_params
+    params.require(:currency).permit(:base, :value, :foreign)
   end
 end
--- spec/controllers/currencies_controller_spec.rb
+++ spec/controllers/currencies_controller_spec.rb
@@ -30,4 +30,51 @@ RSpec.describe CurrenciesController, type: :controller do
       get :show, params: { id: currency.id }
     end
   end
+
+  describe 'GET #exchange' do
+    let(:currency) { FactoryBot.create(:currency) }
+    let(:exchange_params) do
+      {
+        id: currency.id,
+        currency: { base: base, value: value, foreign: foreign }
+      }
+    end
+    let(:base) { currency.source }
+    let(:value) { 1.0 }
+    let(:foreign) { currency.target_rates.sample['target'] }
+
+    let(:currency_exchanger) do
+      instance_double(CurrencyExchanger, foreign_rate_for: 1)
+    end
+
+    before do
+      allow(CurrencyExchanger).to receive(:new).and_return(currency_exchanger)
+    end
+
+    it "returns http success" do
+      get :exchange, xhr: true, params: exchange_params
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'responds with javascript media type' do
+      get :exchange, xhr: true, params: exchange_params
+
+      expect(response.media_type).to eq('text/javascript')
+    end
+
+    it 'initializes service class with source and target_rates' do
+      expect(CurrencyExchanger).to receive(:new)
+        .with(base, currency.target_rates)
+        .and_call_original
+
+      get :exchange, xhr: true, params: exchange_params
+    end
+
+    it 'initialized service class returns foreign rate' do
+      expect(currency_exchanger).to receive(:foreign_rate_for).with(foreign)
+
+      get :exchange, xhr: true, params: exchange_params
+    end
+  end
 end
