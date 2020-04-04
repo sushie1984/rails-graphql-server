54c2e32a473cd33aa182794b724a952b95da5e82
Sascha Burku <sascha_burku@yahoo.de>
2020-04-04 12:27:49 +0200

    Provide currency exchange rate service class

---
+++ app/service/currency_exchanger.rb
@@ -0,0 +1,46 @@
+class UnknownCurrency < StandardError; end
+
+class CurrencyExchanger
+  def initialize(base_currency, foreign_rates)
+    @base_currency = base_currency
+    @foreign_rates = foreign_rates
+  end
+
+  def foreign_rate_for(foreign_currency)
+    return 1.0 if @base_currency == foreign_currency
+    return foreign_rate(foreign_currency) if foreign_rates?(foreign_currency)
+
+    rate_for(foreign_currency) || inverse_rate || unknown_currency_error!
+  end
+
+  private
+
+  def foreign_rate(foreign_currency)
+    rate_for(foreign_currency) / rate_for(@base_currency)
+  end
+
+  def rate_for(currency)
+    (@foreign_rates.find{ |entry| entry['target'] == currency } || {})['rate']
+  end
+
+  def inverse_rate
+    rate = rate_for(@base_currency)
+    return unless rate
+
+    1.0 / rate
+  end
+
+  def unknown_currency_error!
+    raise UnknownCurrency, 'The foreign_currency has no rate defined!'
+  end
+
+  def foreign_rates?(foreign_currency)
+    foreign_currencies.include?(foreign_currency) &&
+      foreign_currencies.include?(@base_currency)
+  end
+
+  def foreign_currencies
+    @foreign_currencies ||=
+      @foreign_rates.map { |foreign_rate| foreign_rate['target'] }
+  end
+end
---
+++ spec/service/currency_exchange_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe CurrencyExchanger do
+  let(:exchanger) { described_class.new(base_currency, foreign_rates) }
+  let(:foreign_rates) { [euro_to_dollar, swiss_franc_to_dollar]}
+  let(:euro_to_dollar) { { 'target' => 'EUR', 'rate' => 0.5 } }
+  let(:swiss_franc_to_dollar) { { 'target' => 'CHF', 'rate' => 0.8 } }
+
+  describe '#foreign_rate_for' do
+    subject(:foreign_rate_for) { exchanger.foreign_rate_for(foreign_currency) }
+    let(:base_currency) { 'USD' }
+    let(:foreign_currency) { 'EUR' }
+    let(:dollar_to_euro_rate) { euro_to_dollar['rate'] }
+
+    it { is_expected.to eq(dollar_to_euro_rate) }
+
+    context 'when swapping base_currency and foreign_currency currency' do
+      let(:base_currency) { 'EUR' }
+      let(:foreign_currency) { 'USD' }
+      let(:euro_to_dollar_rate) { 2.0 }
+
+      it { is_expected.to eq(euro_to_dollar_rate) }
+    end
+
+    context 'with base and foreign_currency being part of foreign_rates' do
+      let(:base_currency) { 'CHF' }
+      let(:foreign_currency) { 'EUR' }
+      let(:swiss_franc_to_euro_rate) { 0.625 }
+
+      it { is_expected.to eq(swiss_franc_to_euro_rate) }
+    end
+
+    context 'with base and foreign_currency being the same' do
+      let(:base_currency) { 'USD' }
+      let(:foreign_currency) { base_currency }
+
+      it { is_expected.to eq(1.0) }
+    end
+
+    context 'with undefined foreign_currency' do
+      let(:foreign_currency) { 'FOO' }
+
+      it 'raises an error' do
+        expect { foreign_rate_for }.to raise_error(UnknownCurrency)
+      end
+    end
+  end
+end
