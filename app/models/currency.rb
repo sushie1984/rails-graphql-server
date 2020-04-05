99bd0c2ccc80a06ce5783ce1fb90a685e33f07cc
Sascha Burku <sascha_burku@yahoo.de>
2020-04-05 16:35:03 +0200

    Improve currency model with an upsert process

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,11 @@
 class Currency < ApplicationRecord
+  def self.upsert_for!(base:, foreign_rates:, published_at:)
+    entry = where(published_at:
+                    (published_at.beginning_of_day..published_at.end_of_day))
+            .first_or_initialize(base: base)
+    entry.foreign_rates = foreign_rates
+    entry.published_at = published_at
+    entry.save!
+    entry
+  end
 end
