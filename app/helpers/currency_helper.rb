e03ac716209560717909063e3bb098bdeac7b833
Sascha Burku <sascha_burku@yahoo.de>
2020-04-06 15:56:13 +0200

    Apply Layout/SpaceInsideArrayLiteralBrackets linter suggestions

--- app/helpers/currency_helper.rb
+++ app/helpers/currency_helper.rb
@@ -1,7 +1,7 @@
 module CurrencyHelper
   def available_options(currency)
-    [ [currency.base, currency.base] ] | currency.foreign_rates.map do |entry|
-      [ entry['currency'], entry['currency'] ]
+    [[currency.base, currency.base]] | currency.foreign_rates.map do |entry|
+      [entry['currency'], entry['currency']]
     end
   end
 end
