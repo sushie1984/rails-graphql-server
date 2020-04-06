113cfebd6d0259e4e5dd07e60021259ed5e0a3bd
Sascha Burku <sascha_burku@yahoo.de>
2020-04-06 15:46:51 +0200

    Apply Layout/BlockAlignment linter suggestions

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -5,21 +5,21 @@ FactoryBot.define do
     foreign_rates {
       [{ 'currency' => 'USD', 'rate' => 1.0977 },
        { 'currency' => 'CHF', 'rate' => 1.0581 }]
-     }
+    }
 
     trait :for_usd_dollar do
       base { 'USD' }
       foreign_rates {
         [{ 'currency' => 'EUR', 'rate' => 0.895375 },
          { 'currency' => 'CHF', 'rate' => 0.951265 }]
-       }
+      }
     end
 
     trait :with_random_target_rates do
       foreign_rates {
         [{ 'currency' => 'USD', 'rate' => Random.rand(1.0..1.2) },
          { 'currency' => 'CHF', 'rate' => Random.rand(1.2..1.4) }]
-       }
+      }
     end
   end
 end
