0135efc20645682f3423ff9b062464f58e257ce4
Sascha Burku <sascha_burku@yahoo.de>
2020-04-06 16:08:07 +0200

    Apply Style/RescueStandardError linter suggestions

--- app/controllers/api/graphql_controller.rb
+++ app/controllers/api/graphql_controller.rb
@@ -12,7 +12,7 @@ module Api
       }
       result = GraphqlServerSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
       render json: result
-    rescue => e
+    rescue StandardError => e
       raise e unless Rails.env.development?
 
       handle_error_in_development e
