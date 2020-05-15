86688e621767bb07350ae5d530d4e49bf5e94c64
Sascha Burku <sascha_burku@yahoo.de>
2020-05-15 17:05:55 +0200

    Highlight menu item for easier navigation

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
   mdc = window.mdc
   const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-  const menuButton = mdc.ripple.MDCRipple.attachTo(document.getElementById('menu'))
+  const menuButton = mdc.ripple.MDCRipple.attachTo(document.getElementById('menu'));
   menuButton.listen('click', () => {
       drawer.open = !drawer.open;
   });
+
+  mdc_list_item_name = document.getElementById('mdc-drawer-navlist').value;
+  const nav_list = drawer.list;
+  item = nav_list.listElements.find(item => item.name == mdc_list_item_name);
+  item.classList.add('mdc-list-item--activated');
 });
