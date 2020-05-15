3182ac3f35c8cf93b5dce1d9439b4080b7510bb5
Sascha Burku <sascha_burku@yahoo.de>
2020-05-15 17:37:55 +0200

    Default open navbar for easier ui experience

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
   mdc = window.mdc
   const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
   const menuButton = mdc.ripple.MDCRipple.attachTo(document.getElementById('menu'));
+  drawer.open = true;
   menuButton.listen('click', () => {
       drawer.open = !drawer.open;
   });
